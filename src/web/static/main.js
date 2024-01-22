cd4060a869027d84dcfebf1716bca0db1a5c8917
DESKTOP-2DOE206\Dexsper <dexsperpro@gmail.com>
2024-01-22 08:57:04 +0600

    Add telegram authentication

--- src/web/static/main.js
+++ src/web/static/main.js
@@ -2,19 +2,29 @@ let tg = window.Telegram.WebApp;
 tg.headerColor = "#9538ec";
 
 export async function fetchData(url, data) {
-    try {
-        const response = await axios.post(url, data);
-        return response.data;
-    } catch (error) {
-        console.error('Error:', error);
-        throw error;
+  try {
+    const response = await axios.post(url, data, {
+      headers: {
+        Authentication: window.Telegram.WebApp.initData,
+      },
+    });
+    return response.data;
+  } catch (error) {
+    if (error.response.status === 401) {
+      showSnackBar("❌ Не авторизован!");
     }
+
+    console.error("Error:", error);
+    throw error;
+  }
 }
 
 export function showSnackBar(text) {
-    let snackbar = document.querySelector(".snackbar");
+  let snackbar = document.querySelector(".snackbar");
 
-    snackbar.style.display = "flex";
-    snackbar.textContent = text;
-    setTimeout(() => {snackbar.style.display = "none"}, 3000);
+  snackbar.style.display = "flex";
+  snackbar.textContent = text;
+  setTimeout(() => {
+    snackbar.style.display = "none";
+  }, 3000);
 }
